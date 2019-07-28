78e57722c7936dfcf2aa85bf56fbeb9e2e3086da
Gabe R <Lockheedmaniac@gmail.com>
2019-07-27 17:33:54 -0700

    fixed up conf.py for docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../CalibrationCode'))
-
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
@@ -50,7 +50,7 @@ autosummary_generate = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
