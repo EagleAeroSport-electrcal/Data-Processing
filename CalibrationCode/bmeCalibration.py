a4049a6f1f18684108965d2ab70386c23d99cfdf
Gabe R <Marsfan@users.noreply.github.com>
2019-08-18 19:44:26 -0500

    fix bmeCalibration to be in line with bosch code

--- CalibrationCode/bmeCalibration.py
+++ CalibrationCode/bmeCalibration.py
@@ -6,46 +6,45 @@ Each Function is passed the necessary calibration coefficents and a raw
 value, and returns a compensated value.
 individually.
 """
-from typing import Tuple, Union, Optional
+from typing import Tuple, Union
 from numpy import uint32 as uint32_t
-from numpy import uint64 as uint64_t
+from numpy import int32 as int32_t
+from numpy import int64 as int64_t
+from CalibrationCode.customObjs import BME280Coefficents
+from CalibrationCode.typeAliases import TempCoefsType, PresCoefsType, HumidityCoefsType
 # TODO add some logging code?
 # Predefine some of the constants
-# TODO Double check how necessary this is
 # TODO Finish adding constants (if needed)
 
 
-TemperatureCoefsType = Tuple[int, int, int]
-PressureCoefsType = Tuple[int, int, int, int, int, int, int, int, int]
-HumidityCoefsType = Tuple[int, int, int, int, int, int]
-CoefsType = Tuple[TemperatureCoefsType, PressureCoefsType, HumidityCoefsType]
-ValsType = Tuple[int, int, int]
-
-
-tempMin: uint32_t = uint32_t(-4000)
-tempMax: uint32_t = uint32_t(8500)
-pressureMin: uint32_t = uint32_t(3000000)
-pressureMax: uint32_t = uint32_t(11000000)
-humidityMax: uint32_t = uint32_t(100000)
-
-
-# Raw Values and calibration coefficents
-# This part calculats tFine and the actual temperature value
-# Based off of line 1014 of the BME280 driver
-
 class CompensateBME280:
-    """Perform BME280 compensation using numpy integers."""
+    """Perform BME280 compensation using numpy integers.
 
-    def __init__(self, coefs: CoefsType, uTemp: int, uPres: int, uHumid: int) -> None:
-        """Set up all of the variables."""
+    Args:
+        coefs (CoefsType): The Calibration Coefficents for the BME 280 as a dictionary containing elements
+            temperature, pressure, and humidity, each of which contains a list of the calibration coefficents.
+        uTemp (int): The uncompensated temperature value.
+        uPres (int): The uncompensated pressure value.
+        uHumid (int): The uncompensated humidity value.
+
+    Attributes
+        temperature (int): The compensated temperature value.
+        tFine (uint32_t): The fine temperature value, used for compensating pressure and humidity.
+        pressure (int): The compensated pressure value.
+        humidity (int): The compensated humidity.
+
+    """
+
+    def __init__(self, coefs: BME280Coefficents, uTemp: int, uPres: int, uHumid: int) -> None:
+        """Initialize an object containing compensated values for the BME280."""    # noqa: I101
         self.temperature: int
         self.tFine: uint32_t
-        self.temperature, self.tFine = self.compensateTemp(uTemp, coefs[0])
-        self.pressure = self.compensatePres(uPres, coefs[1], self.tFine)
-        self.humidity = self.compensateHumid(uHumid, coefs[2], self.tFine)
+        self.temperature, self.tFine = self.compensateTemp(uTemp, coefs.temperature)
+        self.pressure = self.compensatePres(uPres, coefs.pressure, self.tFine)
+        self.humidity = self.compensateHumid(uHumid, coefs.humidity, self.tFine)
 
     @staticmethod
-    def compensateTemp(uTemp: int, tCoefs: TemperatureCoefsType) -> Tuple[int, uint32_t]:
+    def compensateTemp(uTemp: int, tCoefs: TempCoefsType) -> Tuple[int, int32_t]:
         """Convert raw temperature into a useable values.
 
         Args:
@@ -56,18 +55,28 @@ class CompensateBME280:
             Tuple of the calibrated temperature, and the value of tFine
 
         """
-        var1: uint32_t = uint32_t((uTemp / 8) - (tCoefs[0] * 2))
-        var1 = uint32_t((var1 * tCoefs[1]) / 2048)
+        # Derived from Bosch BME280 driver, line 1121
+        tempMin = -4000
+        tempMax = 8500
+        # Raw Values and calibration coefficents
+        # This part calculats tFine and the actual temperature value
+        # Based off of line 1014 of the BME280 driver
+        var1: int32_t = int32_t((uTemp // 8) - (tCoefs[0] * 2))
+        var1 = (var1 * int32_t(tCoefs[1])) // 2048
 
-        var2: uint32_t = uint32_t((uTemp / 16) - (tCoefs[0]))
-        var2 = uint32_t((((var2 * var2) / 4096) * (tCoefs[2])) / 16384)
-        tFine = uint32_t(var1 + var2)
-        temperature: uint32_t = uint32_t((tFine * 5 + 128) / 256)
+        var2: int32_t = int32_t((uTemp // 16) - (int32_t(tCoefs[0])))
+        var2 = (((var2 * var2) // 4096) * int32_t(tCoefs[2])) // 16384
+        tFine = var1 + var2
+        temperature: int32_t = int32_t((tFine * 5 + 128) // 256)
+        if temperature > tempMax:
+            temperature = tempMax
+        elif temperature < tempMin:
+            temperature = tempMin
         return int(temperature), tFine
 
     # This part calculates the actual pressure
     @staticmethod
-    def compensatePres(uPres: int, pCoefs: PressureCoefsType, tFine: uint32_t) -> int:
+    def compensatePres(uPres: int, pCoefs: PresCoefsType, tFine: uint32_t) -> int:
         """Convert the raw pressure values into useable units.
 
         Args:
@@ -79,21 +88,24 @@ class CompensateBME280:
             Integer of the calculated pressure
 
         """
-        var1: uint64_t = uint64_t(tFine - 128000)
-        var2: uint64_t = uint64_t(var1 * var1 * pCoefs[5])
-        var2 = uint64_t(var2 + (var1 * pCoefs[4] * 131072))
-        var2 = uint64_t(var2 + (pCoefs[3] * 34359738368))
-        var1 = uint64_t((var1 * var1 * pCoefs[2] / 256) + (var1 * pCoefs[1] * 4096))
-        var3: uint64_t = uint64_t(1 * 140737488355328)
-        var1 = uint64_t((var3 + var1) * (pCoefs[0] / 8589934592))
+        # Derived from bosch BME280 driver line 1154
+        pressureMin = 3000000
+        pressureMax = 11000000
+        var1: int64_t = int64_t(tFine) - 128000
+        var2: int64_t = var1 * var1 * int64_t(pCoefs[5])
+        var2 = var2 + ((var1 * int64_t(pCoefs[4])) * 131072)
+        var2 = var2 + (int64_t(pCoefs[3]) * 34359738368)
+        var1 = ((var1 * var1 * int64_t(pCoefs[2])) / 256) + (var1 * int64_t(pCoefs[1]) * 4096)
+        var3: int64_t = int64_t(1) * 140737488355328
+        var1 = (var3 + var1) * (int64_t(pCoefs[0]) / 8589934592)
         # Avoids a divide by zero exception for pressure
         if var1:
-            var4: uint64_t = uint64_t(1048576 - uPres)
-            # FIXME Does this need int64_t calling?
-            var4 = uint64_t((((var4 * 2147483648) - var2) * 3125) / var1)
-            var1 = uint64_t((pCoefs[8]) * (var4 / 8192) * (var4 / 8192)) / 33554432
-            var2 = uint64_t((pCoefs[7] * var4) / 524288)
-            var4 = uint64_t((var4 + var1 + var2) / 256 + ((pCoefs[6] * 16)))
+            var4: int64_t = int64_t(1048576 - uPres)
+            # FIXME Make sure int64_C is compatible with int64_t
+            var4 = (((var4 * int64_t(2147483648)) - var2) * 3125) / var1
+            var1 = (int64_t(pCoefs[8]) * (var4 / 8192) * (var4 / 8192)) / 33554432
+            var2 = (int64_t(pCoefs[7]) * var4) / 524288
+            var4 = ((var4 + var1 + var2) / 256) + (int64_t(pCoefs[6]) * 16)
             pressure: uint32_t = uint32_t(((var4 / 2) * 100) / 128)
 
             # Compensate for risks of exceedig min and max pressure
@@ -107,7 +119,7 @@ class CompensateBME280:
 
     # This part compensates the humindity.
     @staticmethod
-    def compensateHumid(uHumid: int, hCoefs: HumidityCoefsType, tFine: Union[float, int]) -> int:
+    def compensateHumid(uHumid: int, hCoefs: HumidityCoefsType, tFine: int32_t) -> int:
         """Compensates the raw humidity values, making it user readable.
 
         Args:
@@ -119,97 +131,117 @@ class CompensateBME280:
             Compensated humidity value
 
         """
-        var1: uint32_t = uint32_t(tFine - 76800)
-        var2: uint32_t = uint32_t(uHumid * 16384)
-        var3: uint32_t = uint32_t(hCoefs[3] * 1048576)
-        var4: uint32_t = uint32_t(hCoefs[4] * var1)
-        var5: uint32_t = uint32_t((((var2 - var3) - var4) + 16384) / 32768)
-        var2 = uint32_t(var1 * (hCoefs[5] / 32768))
-        var3 = uint32_t((var1 * (hCoefs[2] / 2048)))
-        var4 = uint32_t((var2 * (var3 + 32768) / 1024) + 2097152)
-        var2 = uint32_t(((var4 * (hCoefs[1])) + 8192) / 16384)
-        var3 = uint32_t(var5 * var2)
-        var4 = uint32_t(((var3 / 32768) * (var3 / 32768)) / 128)
-        var5 = uint32_t(var3 - ((var4 * hCoefs[0]) / 16))
+        # Derived from bosch BME280 Driver line 1263
+        humidityMax = uint32_t(102400)
+        var1: int32_t = int32_t(tFine - 76800)
+        var2: int32_t = int32_t(uHumid * 16384)
+        var3: int32_t = int32_t(int32_t(hCoefs[3]) * 1048576)
+        var4: int32_t = int32_t(hCoefs[4]) * var1
+        var5: int32_t = (((var2 - var3) - var4) + 16384) / 32768
+        var2 = (var1 * int32_t(hCoefs[5])) / 1024
+        var3 = (var1 * int32_t(hCoefs[2])) / 2048
+        var4 = ((var2 * (var3 + 32768)) / 1024) + 2097152
+        var2 = ((var4 * (int32_t(hCoefs[1]))) + 8192) / 16384
+        var3 = var5 * var2
+        var4 = ((var3 / 32768) * (var3 / 32768)) / 128
+        var5 = var3 - ((var4 * int32_t(hCoefs[0])) / 16)
         if var5 < 0:
-            var5 = uint32_t(0)
+            var5 = int32_t(0)
         elif var5 > 419430400:
-            var5 = uint32_t(419430400)
+            var5 = int32_t(419430400)
 
-        humidity = uint32_t(var5 / 4096)
+        humidity: uint32_t = uint32_t(var5 / 4096)
         if humidity > humidityMax:
             humidity = humidityMax
         return int(humidity)
 
 
 class CompensateBME280Native:
-    """Perform BME280 compensation using python native int and float."""
+    """Perform BME280 compensation using python native int and float.
 
-    def __init__(self, coefs: CoefsType, uTemp: int, uPres: int, uHumid: int) -> None:
-        """Initialize Instance."""
-        self.tCoefs: TemperatureCoefsType = coefs[0]
-        self.pCoefs: PressureCoefsType = coefs[1]
-        self.hCoefs: HumidityCoefsType = coefs[2]
-        self.uTemp: int = uTemp
-        self.uPres: int = uPres
-        self.uHumid: int = uHumid
-        self.tFine: uint32_t = self.compensateTemp(self.uTemp, self.tCoefs)[1]
+    Args:
+        coefs (CoefsType): The Calibration Coefficents for the BME 280 as a dictionary containing elements
+            temperature, pressure, and humidity, each of which contains a list of the calibration coefficents.
+        uTemp (int): The uncompensated temperature value.
+        uPres (int): The uncompensated pressure value.
+        uHumid (int): The uncompensated humidity value.
 
-    def compensateAll(self, coefs: Optional[CoefsType] = None, uncompVals: Optional[ValsType] = None) -> ValsType:
-        """Calculate all compensation values for the bme 280."""
-        temperature: int
-        tFine: uint32_t
-        pressure: int
-        humidity: int
+    Attributes
+        temperature (int): The compensated temperature value.
+        tFine (uint32_t): The fine temperature value, used for compensating pressure and humidity.
+        pressure (int): The compensated pressure value.
+        humidity (int): The compensated humidity.
 
-        if coefs is not None and uncompVals is not None:
-            temperature, tFine = self.compensateTemp(uncompVals[0], coefs[0])
-            pressure = self.compensatePres(uncompVals[1], coefs[1], tFine)
-            humidity = self.compensateHumid(uncompVals[2], coefs[2], tFine)
-        else:
-            temperature, tFine = self.compensateTemp(self.uTemp, self.tCoefs)
-            pressure = self.compensatePres(self.uPres, self.pCoefs, tFine)
-            humidity = self.compensateHumid(self.uHumid, self.hCoefs, tFine)
-        return(temperature, pressure, humidity)
+    """
 
-    def compensateTemp(self, uTemp: int, tCoefs: TemperatureCoefsType,) -> Tuple[int, Union[int, float]]:
-        """Convert raw temperature into a useable values."""
-        if uTemp is None or tCoefs is None:
-            uTemp = self.uTemp
-            tCoefs = self.tCoefs
-            tFine = self.tFine
+    def __init__(self, coefs: BME280Coefficents, uTemp: int, uPres: int, uHumid: int) -> None:
+        """Initialize Instance."""  # noqa: I101
+        self.temperature: float
+        self.tFine: uint32_t
+        self.temperature, self.tFine = self.compensateTemp(uTemp, coefs.temperature)
+        self.pressure = self.compensatePres(uPres, coefs.pressure, self.tFine)
+        self.humidity = self.compensateHumid(uHumid, coefs.humidity, self.tFine)
 
-        var1: uint32_t = (uTemp / 8) - (tCoefs[0] * 2)
-        var1 = (var1 * tCoefs[1]) / 2048
+    @staticmethod
+    def compensateTemp(uTemp: int, tCoefs: TempCoefsType) -> Tuple[float, float]:
+        """Convert raw temperature into a useable values.
 
-        var2: uint32_t = (uTemp / 16) - (tCoefs[0])
-        var2 = (((var2 * var2) / 4096) * (tCoefs[2])) / 16384
-        tFine = var1 + var2
-        temperature: uint32_t = (tFine * 5 + 128) / 256
+        Args:
+            uTemp: The uncompensated temperature value
+            tCoefs: The calibration coefficents
+
+        Returns:
+            Tuple of the calibrated temperature, and the value of tFine
+
+        """
+        # Derived from bosch BME280 Driver line 1005
+        # Double Checked against Adafruit Python BME280 line 212
+        tempMin = -40
+        tempMax = 85
+        var1: float = (uTemp) / 16384 - (tCoefs[0]) / 1024
+        var1 = var1 * tCoefs[1]
+
+        var2: float = uTemp / 131072 - tCoefs[0] / 8192
+        var2 = (var2 * var2) * tCoefs[2]
+        tFine = int(var1 + var2)
+        temperature: float = (var1 + var2) / 5120
+        if temperature < tempMin:
+            temperature = tempMin
+        elif temperature > tempMax:
+            temperature = tempMax
         return temperature, tFine
 
-    def compensatePres(self, uPres: int, pCoefs: PressureCoefsType, tFine: uint32_t) -> int:
-        """Convert the raw pressure values into useable units."""
-        if uPres is None or pCoefs is None:
-            uPres = self.uPres
-            pCoefs = self.pCoefs
-            tFine = self.tFine
-        var1: uint64_t = tFine - 128000
-        var2: uint64_t = var1 * var1 * pCoefs[5]
-        var2 = var2 + (var1 * pCoefs[4] * 131072)
-        var2 = var2 + (pCoefs[3] * 34359738368)
-        var1 = (var1 * var1 * pCoefs[2] / 256) + (var1 * pCoefs[1] * 4096)
-        var3: uint64_t = 1 * 140737488355328
-        var1 = (var3 + var1) * (pCoefs[0] / 8589934592)
+    @staticmethod
+    def compensatePres(uPres: int, pCoefs: PresCoefsType, tFine: int) -> float:
+        """Convert the raw pressure values into useable units.
+
+        Args:
+            uPres: The uncompensated Pressure Values
+            pCoefs: Tuple of pressure compensation coefficents
+            tFine: Fine temperature calculation from compensateTemp
+
+        Returns:
+            Integer of the calculated pressure
+
+        """
+        pressureMin = 30000
+        pressureMax = 110000
+        # Derived from line 1035 of BME280 Bosch Driver
+        # Double Checked against Adafruit Python BME280 line 223
+        var1: float = (tFine / 2) - 64000
+        var2: float = var1 * var1 * pCoefs[5] / 32768
+        var2 = var2 + var1 * pCoefs[4] * 2
+        var2 = (var2 / 4) + (pCoefs[3] * 65536)
+        var3: float = pCoefs[2] * var1 * var1 / 524288
+        var1 = (var3 + pCoefs[1] + var1) / 524288
+        var1 = (1 + var1 + 32768) * pCoefs[0]
         # Avoids a divide by zero exception for pressure
         if var1:
-            var4: uint64_t = 1048576 - uPres
-            # FIXME Does this need int64_t calling?
-            var4 = (((var4 * 2147483648) - var2) * 3125) / var1
-            var1 = (pCoefs[8]) * (var4 / 8192) * (var4 / 8192) / 33554432
-            var2 = (pCoefs[7] * var4) / 524288
-            var4 = (var4 + var1 + var2) / 256 + ((pCoefs[6] * 16))
-            pressure: uint32_t = ((var4 / 2) * 100) / 128
+            pressure: float = 1048576 - uPres
+            pressure = (pressure - (var2 / 4096)) * 6250 / var1
+            var1 = (pCoefs[8]) * pressure * pressure / 2147483648
+            var2 = pressure * pCoefs[7] / 32768
+            pressure = pressure + (var1 + var2 + pCoefs[6]) / 16
 
             # Compensate for risks of exceedig min and max pressure
             if pressure < pressureMin:
@@ -218,32 +250,34 @@ class CompensateBME280Native:
                 pressure = pressureMax
         else:
             pressure = pressureMin
-        return int(pressure)
+        return pressure
 
-    def compensateHumid(self, uHumid: int, hCoefs: HumidityCoefsType, tFine: Union[float, int]) -> int:
-        """Compensates the raw humidity values, making it user readable."""
-        if uHumid is None or hCoefs is None:
-            uHumid = self.uHumid
-            hCoefs = self.hCoefs
-            tFine = self.tFine
-        var1: uint32_t = tFine - 76800
-        var2: uint32_t = uHumid * 16384
-        var3: uint32_t = hCoefs[3] * 1048576
-        var4: uint32_t = hCoefs[4] * var1
-        var5: uint32_t = (((var2 - var3) - var4) + 16384) / 32768
-        var2 = var1 * (hCoefs[5] / 32768)
-        var3 = (var1 * (hCoefs[2] / 2048))
-        var4 = (var2 * (var3 + 32768) / 1024) + 2097152
-        var2 = ((var4 * (hCoefs[1])) + 8192) / 16384
-        var3 = var5 * var2
-        var4 = ((var3 / 32768) * (var3 / 32768)) / 128
-        var5 = var3 - ((var4 * hCoefs[0]) / 16)
-        if var5 < 0:
-            var5 = 0
-        elif var5 > 419430400:
-            var5 = 419430400
+    @staticmethod
+    def compensateHumid(uHumid: int, hCoefs: HumidityCoefsType, tFine: Union[float, int]) -> float:
+        """Compensates the raw humidity values, making it user readable.
 
-        humidity = var5 / 4096
+        Args:
+            uHumid: The uncompensated humidity value
+            hCoefs: Tuple with the compensation coefficents
+            tFine: Fine temperature value from compensateTemp
+
+        Returns:
+            Compensated humidity value
+
+        """
+        # Derived from Bosch BME280 Driver line 1082
+        humidityMax = 100
+        humidityMin = 0
+        var1: float = tFine - 76800
+        var2: float = hCoefs[3] * 64 * (hCoefs[4] / 16384.0 * var1)
+        var3: float = uHumid - var2
+        var4: float = hCoefs[1] / 65536
+        var5: float = (1 + ((hCoefs[2]) / 67108864) * var1)
+        var6: float = 1 + ((hCoefs[5]) / 67108864) * var1 * var5
+        var6 = var3 * var4 * (var5 * var6)
+        humidity: float = var6 * (1.0 - hCoefs[0] * var6 / 524288)
         if humidity > humidityMax:
             humidity = humidityMax
-        return int(humidity)
+        elif humidity < humidityMin:
+            humidity = humidityMin
+        return humidity
