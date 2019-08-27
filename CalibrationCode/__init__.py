2ead7c7b09978065934aac76d871e30884cd18b7
Gabe R <Marsfan@users.noreply.github.com>
2019-08-27 12:19:40 -0700

    converted to function as module

---
+++ CalibrationCode/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import CalibrationCode.amsCalibration as amsCalibration     # noqa: F401
+import CalibrationCode.bmeCalibration as bmeCalibration     # noqa: F401
+import CalibrationCode.dataExtraction as dataExtraction     # noqa: F401
+from CalibrationCode.customObjs import BME280Coefficents    # noqa: F401
